04619e331c82dade6735bf50afae6db22fdef0d5
Maciej Zimnoch <maciej@scylladb.com>
2023-02-12 21:11:06 +0100

    Add algorithms util package

---
+++ pkg/util/constraints/constraints.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2023 ScyllaDB.
+
+package constraints
+
+// Unsigned is a constraint that any unsigned integer type satisfies.
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+// Signed is a constraint that any signed integer type satisfies.
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+// Integer is a constraint that any integer type satisfies.
+type Integer interface {
+	Unsigned | Signed
+}
+
+// Float is a constraint that any floating type satisfies.
+type Float interface {
+	~float32 | ~float64
+}
+
+// Number is a constraint that any numeric type satisfies.
+type Number interface {
+	Integer | Float
+}
+
+// Ordered is a constraint that any type which supports ordering or equality operator satisfies.
+type Ordered interface {
+	Integer | Float | ~string
+}
